b1f55e26200aae7d2f846cba64d188a00ae68cec
marudor <marudor@chaosdorf.de>
2015-04-12 19:47:13 +0200

    foo

--- src/index.js
+++ src/index.js
@@ -10,20 +10,28 @@ const express = require('express'),
 
 app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
 app.set('view engine', 'hbs');
-app.listen(61182);
+app.listen(62601);
 
 
 app.get('/', (req, res) => {
   var episodes = fs.readdirSync('../episodes');
   var e1 = [];
-  episodes = _.each(episodes, e => { 
-    if (e.indexOf('.json') !== -1) {
-      var m = require('../../episodes/'+e);
+  episodes = _.map(episodes, e => {
+	  if (e.indexOf('.json') !== -1) {
+	  	return require('../../episodes/'+e);
+	}
+  });
+  episodes = _.without(episodes, undefined);
+  _.each(episodes, m => {
+	  m.timestamp = moment(m.rawTweet.created_at);
+  });
+  episodes = _.sortBy(episodes, 'timestamp');
+
+  _.each(episodes, m => { 
       m.text = m.text.replace(m.rawTweet.entities.media[0].url,'');
       m.text = m.text.replace('#Einhornkätzchen', 'Einhornkätzchen');
-      m.date = moment(m.rawTweet.created_at).format('DD.MM.YYYY');
+      m.date = m.timestamp.format('DD.MM.YYYY HH:mm');
       e1.push(m);
-    }
   });
   res.render('index', {
     episodes: e1
